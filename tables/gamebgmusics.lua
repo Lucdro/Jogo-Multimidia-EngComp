e9f34e3788ceb8d0a6a047f6c3577076402a4f62
Lucdro <87252446+Lucdro@users.noreply.github.com>
2023-06-04 00:47:08 -0300

    more music

--- tables/gamebgmusics.lua
+++ tables/gamebgmusics.lua
@@ -7,4 +7,8 @@ table.insert(BGMusics,love.audio.newSource("sounds/19 Candy Mountain.mp3","stati
 table.insert(BGMusics,love.audio.newSource("sounds/05 Slider.mp3","static"))
 table.insert(BGMusics,love.audio.newSource("sounds/20 Metallic Mario.mp3","static"))
 table.insert(BGMusics,love.audio.newSource("sounds/08 Mad Forest.mp3","static"))
+table.insert(BGMusics,love.audio.newSource("sounds/PlantsVs.Zombies-Loonboon(ColomboGMGS2).mp3","static"))
+table.insert(BGMusics,love.audio.newSource("sounds/Kirby dream land theme song.mp3","static"))
+table.insert(BGMusics,love.audio.newSource("sounds/59. Poké Mart.mp3","static"))
+table.insert(BGMusics,love.audio.newSource("sounds/45. Battle! (Gym Leader).mp3","static"))
 
